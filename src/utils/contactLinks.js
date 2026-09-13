dcd802ec5766cfe6ac31420bb99edd26e1759c15
sleeplessLh <lawrancehii12345@gmail.com>
2026-09-13 22:50:53 +0800

    Replace failed cutout with portrait panel

--- src/utils/contactLinks.js
+++ src/utils/contactLinks.js
@@ -1,3 +1,8 @@
 export const hostEmail = 'lawrancehii12345@gmail.com';
 export const gmailComposeUrl = email => `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(email || hostEmail)}`;
 export const mailtoUrl = email => `mailto:${email || hostEmail}`;
+export function openEmail(event, email = hostEmail) {
+  event.preventDefault();
+  const popup = window.open(gmailComposeUrl(email), '_blank', 'noopener,noreferrer');
+  if (!popup) window.location.href = mailtoUrl(email);
+}
